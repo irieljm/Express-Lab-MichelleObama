36f475047690a82980c7b00fe9abb5213bb9e293
Iriel Aloni <irieljm@gmail.com>
2020-11-12 11:55:47 +0100

    First commit

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 const express = require('express');
 const app = express();
+const handlebars = require('hbs');
 
 // this makes the public folder available in the html files
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (request, response) => {
-    console.log('this is the home page');
+    //console.log('this is the home page');
     //response.send('<h1>hello from Express</h1>');
-    response.sendFile(__dirname + '/views/home-page.html');
+    response.sendFile(__dirname + '/views/home-page');
 });
 
 app.get('/gallery', (req, res) => {
-    res.sendFile(__dirname + '/views/gallery2.html');
+    res.sendFile(__dirname + '/views/gallery2');
 });
 app.get('/about', (req, res) => {
-    res.sendFile(__dirname + '/views/about.html');
+    res.sendFile(__dirname + '/views/about');
 });
 app.get('/works', (req, res) => {
-    res.sendFile(__dirname + '/views/works.html');
+    res.sendFile(__dirname + '/views/works');
 });
 
 app.get('/url', (req, res) => {
