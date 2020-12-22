6b75d11fbf9bdafdee02909e523121c881a4954a
Iriel Aloni <irieljm@gmail.com>
2020-12-22 21:26:33 +0100

    minor css changes

--- app.js
+++ app.js
@@ -7,17 +7,17 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', (request, response) => {
     console.log('this is the home page');
     //response.send('<h1>hello from Express</h1>');
-    response.sendFile(__dirname + '/home-page.html');
+    response.sendFile(__dirname + '/views/home-page.html');
 });
 
 app.get('/gallery', (req, res) => {
-    res.sendFile(__dirname + '/gallery2.html');
+    res.sendFile(__dirname + '/views/gallery2.html');
 });
 app.get('/about', (req, res) => {
-    res.sendFile(__dirname + '/about.html');
+    res.sendFile(__dirname + '/views/about.html');
 });
 app.get('/works', (req, res) => {
-    res.sendFile(__dirname + '/works.html');
+    res.sendFile(__dirname + '/views/works.html');
 });
 
 app.get('/url', (req, res) => {
